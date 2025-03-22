746d035cd6bf27de6e2c887724f4ed35dc9f0299
wichitoSP <99522188+wichitoSP@users.noreply.github.com>
2025-03-21 21:09:30 -0600

    Se crea el menú para el programa y se coloca la variable opcion para el menú

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,19 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
         double Iva = 0.12;
-        
+
+        while (true){
+
+            System.out.println("***Menú Inventario****");
+            System.out.println("1.)Agregar Producto");
+            System.out.println("2.)Mostrar Producto");
+            System.out.println("3.)Actualizar Cantidad de un Producto");
+            System.out.println("4.)Elimiar un producto");
+            System.out.println("5.)Salir");
+            System.out.println("Seleccione una opción del menu: ");
+            int opcion = scanner.nextInt();
+        }
+
 
 
 
