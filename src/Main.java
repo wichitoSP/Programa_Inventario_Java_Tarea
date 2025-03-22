d9a617951e08b2ec3a18229808c6729c488bad0f
wichitoSP <99522188+wichitoSP@users.noreply.github.com>
2025-03-21 21:04:14 -0600

    Se añade la libreria Scanner y se declara la constante del Iva

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
 
-
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        double Iva = 0.12;
+        
 
 
 
